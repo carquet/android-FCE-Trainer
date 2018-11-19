0cffebf57c80aeab3d0323b2db90d96e6c3394ff
L.Carquet <laetitiacarquet@gmail.com>
2018-11-19 10:54:53 +0100

    adds array of category and iterate through them

--- app/src/main/java/com/example/android/fcetrainer/MainActivity.java
+++ app/src/main/java/com/example/android/fcetrainer/MainActivity.java
@@ -1,18 +1,17 @@
 package com.example.android.fcetrainer;
 
 import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.DragEvent;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    TextView speakingTextView;
-    TextView writingTextView;
-    TextView readingTextView;
-    TextView listeningTextView;
+ public class MainActivity extends AppCompatActivity {
+    LinearLayout mainRoot;
 
 
     @Override
@@ -20,20 +19,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //assigning textview
-        speakingTextView = findViewById(R.id.speaking_textview);
-        writingTextView = findViewById(R.id.writing_textview);
-        readingTextView = findViewById(R.id.reading_textview);
-        listeningTextView = findViewById(R.id.listening_textview);
+        mainRoot = findViewById(R.id.main_root);
+
+        String[] categories = new String[4];
+        categories[0] = "speaking";
+        categories[1] = "writing";
+        categories[2] = "listening";
+        categories[3] = "reading";
+
+        for (final String category1 : categories) {
+            TextView categoryView = new TextView(this);
+            categoryView.setText(category1);
+            categoryView.setOnClickListener((new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    /*Intent i = new Intent(MainActivity.this, SpeakingActivity.class);
+                    startActivity(i);*/
+                    Toast.makeText(getApplicationContext(), "this is index " + category1, Toast.LENGTH_LONG).show();
+                }
+            }));
+            mainRoot.addView(categoryView);
+
+        }
 
 
-        speakingTextView.setOnClickListener((new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, SpeakingActivity.class);
-                startActivity(i);
-            }
-        }));
+        /*speakingTextView.setOnClickListener(); */
 
     }
 }
